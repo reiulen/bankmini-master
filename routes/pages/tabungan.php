93ef06988852c723bf392f36cda0b27fd656f415
reihanpraja <reiandika10@gmail.com>
2022-03-05 22:13:24 +0700

    pembaruan

--- routes/pages/tabungan.php
+++ routes/pages/tabungan.php
@@ -6,6 +6,4 @@ Route::group(['prefix' => 'siswa/tabungan', 'as' => 'tabungan.'], function() {
     Route::get('/', [TabunganController::class, 'index'])->name('index');
     Route::get('/tambahtabungan', [TabunganController::class, 'create'])->name('create');
     Route::get('/{tabungan:id}/edittabungan', [TabunganController::class, 'edit'])->name('edit');
-
-
 });

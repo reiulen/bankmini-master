45a1c1d77e19894da31a77d1da47e0f9cf25170f
reihanpraja <reiandika10@gmail.com>
2022-03-31 05:59:52 +0700

    update

--- resources/views/backend/laporantunggakan/index.blade.php
+++ resources/views/backend/laporantunggakan/index.blade.php
@@ -72,7 +72,7 @@
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Filter Siswa</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Filter Tunggakan</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
